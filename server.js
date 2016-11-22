10776c60bc78fbf86525af4710eec6a2b4eb7982
Dannyandstuff <danielthomasrobinson@gmail.com>
2016-11-22 18:48:19 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,3 @@
-var express = require('express')
-var Audiosearch = require('audiosearch-client-node');
-
-var app = express()
-var audiosearch = new Audiosearch('6e882135d41f23eb8712aa543af7d9e4e9b960e21c2b97403620b7bbe220a17b', '6e4c677ff0f890d001d2257e72f7c05b0a17b1c9b6b1413a950ef534f7e8868a');
-
-app.get('/api/search', function(req, res) {
-    audiosearch.searchShows('Harmontown').then(function (results) {
-        res.json(results);
-    })
-})
-
-app.listen(3000);
-
-
 var express = require('express');
 var Audiosearch = require('audiosearch-client-node');
 var app = express();
