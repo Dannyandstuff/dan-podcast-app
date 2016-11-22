c26562e1fb39320bc7f95ccc7c6e0019d3e750db
Dannyandstuff <danielthomasrobinson@gmail.com>
2016-11-22 18:43:51 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -4,11 +4,27 @@ var Audiosearch = require('audiosearch-client-node');
 var app = express()
 var audiosearch = new Audiosearch('6e882135d41f23eb8712aa543af7d9e4e9b960e21c2b97403620b7bbe220a17b', '6e4c677ff0f890d001d2257e72f7c05b0a17b1c9b6b1413a950ef534f7e8868a');
 
-app.set('port', (process.env.PORT || 3000));
-
 app.get('/api/search', function(req, res) {
     audiosearch.searchShows('Harmontown').then(function (results) {
         res.json(results);
+    })
+})
+
+app.listen(3000);
+
+
+var express = require('express');
+var Audiosearch = require('audiosearch-client-node');
+var app = express();
+var audiosearch = new Audiosearch('6e882135d41f23eb8712aa543af7d9e4e9b960e21c2b97403620b7bbe220a17b', '6e4c677ff0f890d001d2257e72f7c05b0a17b1c9b6b1413a950ef534f7e8868a');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/search', function(request, response) {
+    audiosearch.searchShows('Harmontown').then(function (results) {
+        response.json(results);
     })
 })
 
